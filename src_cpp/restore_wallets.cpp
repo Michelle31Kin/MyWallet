cc164721a8a7f2e9867a0dbf5a1fb3ff100c2d44
Enam A.-M. KODJOH-KPAKPASSOU <eakodjoh-kpakpassou@st.ug.edu.gh>
2025-08-16 14:12:32 +0000

    Update restore_wallets.cpp

--- src_cpp/restore_wallets.cpp
+++ src_cpp/restore_wallets.cpp
@@ -1,22 +1,33 @@
 #include "../include.cpp/my.h"
 
-std::string WalletManager::restore_wallet(std::string &to_restore)
+std::string WalletManager::restore_wallet(const std::string &to_restore)
 {
     sqlite3 *db = WalletManager::init_db();
     sqlite3_stmt *restoreStmt = nullptr;
+    sqlite3_stmt *restoreTransactionStmt = nullptr;
     std::string result = "Failed to restore wallet!";
     
 
-    const char* restoreWalletSQL = "UPDATE wallets SET is_deleted = 0 WHERE name = ?;";
+    const char* restoreWalletSQL = "UPDATE wallets SET is_active = TRUE WHERE name = ?;";
+    const char* restoreRelatedTransactionsSQL = "UPDATE transactions SET is_archived = FALSE WHERE name = ?;";
     
     if (sqlite3_prepare_v2(db, restoreWalletSQL, -1, &restoreStmt, nullptr) != SQLITE_OK) {
         std::cerr << "SQL prepare error: " << sqlite3_errmsg(db) << std::endl;
         goto cleanup;
     }
+    if (sqlite3_prepare_v2(db, restoreRelatedTransactionsSQL, -1, &restoreTransactionStmt, nullptr) != SQLITE_OK) {
+        std::cerr << "SQL prepare error: " << sqlite3_errmsg(db) << std::endl;
+        goto cleanup;
+    }
     
-    sqlite3_bind_text(restoreStmt, 1, to_restore.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(restoreStmt, 1, to_restore.c_str(), -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(restoreTransactionStmt, 1, to_restore.c_str(), -1, SQLITE_TRANSIENT);
     
     if (sqlite3_step(restoreStmt) != SQLITE_DONE) {
+        std::cerr << "Failed to complete the query: " << sqlite3_errmsg(db) << std::endl;
+        goto cleanup;
+    }
+    if (sqlite3_step(restoreTransactionStmt) != SQLITE_DONE) {
         std::cerr << "Failed to complete the query: " << sqlite3_errmsg(db) << std::endl;
         goto cleanup;
     }
@@ -25,6 +36,7 @@ std::string WalletManager::restore_wallet(std::string &to_restore)
     
 cleanup:
     if (restoreStmt) sqlite3_finalize(restoreStmt);
+    if (restoreTransactionStmt) sqlite3_finalize(restoreTransactionStmt);
     WalletManager::closedb(db);
     return result;
 }
