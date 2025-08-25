0871648d863fe210be2e1770775f049f68ac9072
EnamAntoineM <eakodjoh-kpakpassou@st.ug.edu.gh>
2025-08-25 13:14:18 +0000

    Starting frontend project structure crafting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
+<!--In this file, we are creating the base layout for our application
+It will be displayed across all the pages of our application.
+This includes
+
+-->
+
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import appIcon from '$lib/assets/icon.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={appIcon} />
 </svelte:head>
 
 {@render children?.()}
---
+++ src/routes/aboutUs/+page.svelte
@@ -0,0 +1,3 @@
+<main>
+    <p class = font-bold >This is the about us page</p>
+</main>
