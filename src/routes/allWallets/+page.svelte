4778425606321743d4c66262e5d7eb74ee00db71
SARFOMichelle <michellesarfo06@icloud.com>
2025-08-27 13:22:49 +0000

    second pr

--- src/routes/allWallets/+page.svelte
+++ src/routes/allWallets/+page.svelte
@@ -1,3 +1,3 @@
 <main>
-    <p class = "font-extrabold justify-center shadow-2xl text-7xl p-64">This is the all wallets page</p>
+    <p class = "font-bold justify-center shadow-2xl text-7xl p-64">This is the all wallets page</p>
 </main>
