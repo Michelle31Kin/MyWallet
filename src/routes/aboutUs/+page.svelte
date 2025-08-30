7e26d35ad5d25f0ba8e953effdddfda35b08a57b
EnamAntoineM <eakodjoh-kpakpassou@st.ug.edu.gh>
2025-08-30 23:13:35 +0000

    patches to the about us page

--- src/routes/aboutUs/+page.svelte
+++ src/routes/aboutUs/+page.svelte
@@ -1,145 +1,220 @@
+<script lang="ts">
+	import EmmaPic from "$lib/assets/Emma (Edited).jpg";
+	import EnamPic from "$lib/assets/EnamPic (Edited).jpg";
+	import JosephPic from "$lib/assets/Joseph.jpg";
+</script>
+
 <main>
-  <div class="relative container shadow-lg m-4 p-4 min-w-full overflow-hidden min-h-[1500px]">
-    <!-- Background image and blur overlay as background layer -->
-    <div
-      class="absolute inset-0 z-0"
-      style="background-image: url('/src/lib/assets/Background fill.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; animate-bounce"
-    >
-      <div class="absolute inset-0 bg-white/90 sm:backdrop-opacity-10 backdrop-blur-md backdrop-saturate-50"></div>
-    </div>
+	<div class="shadow-lg overflow-hidden">
+		<!-- Background image and blur overlay as background layer -->
+		<div
+			class="absolute inset-0 z-0"
+			style="background-image: url('/src/lib/assets/Background fill.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; animate-bounce"
+		>
+			<div class="absolute inset-0 bg-base-100/80"></div>
+		</div>
 
-    <!-- All content sits above the overlay -->
-    <div class="relative z-10">
-      <div class="justify-center p-16 m-2">
-        <h1 class="
-         text-6xl text-center font-[Noto_serif] font-italic bg-gradient-to-r from-yellow-8000 via-yellow-600 to-yellow-400 bg-clip-text text-transparent drop-shadow-xl 
-        animate-fadeIn tracking-normal  transition-opacity duration-1000 ease-in-out animate-pulseScale">
-          We are a team of passionate individuals with diverse skills, working together to push boundaries <br/> 
-           We are a team of passionate individuals with diverse skills, working together to push boundaries <br/>
-           to <em>cause</em> an impact.
-        </h1>
-        <div>
-          <p class="text-black text-lg-8xl pt-10 text-center leading-relaxed drop-shadow-lg
-           tracking-widest  mb-16 antialiased font-light">
-            Our mission is to deliver exceptional solutions that empower our clients and
-            make a meaningful difference in their lives. <br/>
-          Whether it is through
-            cutting-edge ideas, excellent customer care, or dedication to continuous
-            growth, we strive to set a standard that inspires others. <br/> We believe in creating lasting value and positive change.
-            
-          </p>
-        </div>
-      </div>
+		<!-- All content sits above the overlay -->
+		<div class="relative z-10 mt-16 mb-10">
+			<div class="justify-center">
+				<h1 class=" text-6xl text-center text-warning">
+					We are a team of passionate individuals with diverse skills,
+					working together to push boundaries to <em>cause</em> an
+					impact. <br />
+				</h1>
+				<div>
+					<p
+						class="text-base-content text-lg-8xl pt-6 text-center tracking-widest font-light"
+					>
+						Our mission is to help people take control of their
+						money with clarity and confidence. <br />
+						By making it simple to track wallets, record transactions,
+						and monitor balances, we empower users to stay organized
+						and make smarter financial decisions.
+					</p>
+				</div>
+			</div>
 
-      <div class="flex justify-center mb-12">
-           <button class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-3 px-8 rounded shadow-lg transition duration-300 drop-shadow-9xl">
-               Join our team &gt;&gt;
-          </button>
-       </div>    
-                   
-                   
+			<!-- Images and names of people -->
+			<div>
+				<p
+					class="text-base-content text-2xl text-center font-sans font-semibold mt-6 mb-4"
+				>
+					Meet Our Team Members.
+				</p>
+				<div
+					class="grid md:grid-cols-1 md:grid-rows-8 lg:grid-cols-1 lg:grid-rows-8 xl:grid-rows-4 xl:grid-cols-2 2xl:grid-rows-3 2xl:grid-cols-3 gap-4 pt-7 ml-16 justify-center"
+				>
+					<div
+						class="card card-sm rounded-2xl bg-base-100 w-96 max-h-100 shadow-sm"
+					>
+						<figure class="relative rounded-2xl min-h-full">
+							<img
+								src={EnamPic}
+								alt="Team member"
+								class="object-cover mt-7"
+							/>
+							<div
+								class="absolute bottom-0 w-90 bg-base-300/90 mb-2 rounded-2xl p-3 text-center"
+							>
+								<h2 class="font-semibold text-lg">
+									ENAM A.-M. KODJOH-KPAKPASSOU
+								</h2>
+								<p class="text-sm mt-1">
+									Full-Stack Developer and Group Leader
+								</p>
+							</div>
+						</figure>
+					</div>
 
+					<div
+						class="card card-sm rounded-2xl bg-base-100 w-96 max-h-100 shadow-sm"
+					>
+						<figure class="relative rounded-2xl min-h-full">
+							<img
+								src={EmmaPic}
+								alt="Team member"
+								class="object-cover mt-4"
+							/>
+							<div
+								class="absolute bottom-0 w-90 bg-base-300/90 mb-2 rounded-2xl p-3 text-center"
+							>
+								<h2 class="font-semibold text-lg">
+									ACQUAH, EMMANUEL A. K.
+								</h2>
+								<p class="text-sm mt-1">Frontend Developer</p>
+							</div>
+						</figure>
+					</div>
+					<!-- card 1 -->
+					<div
+						class="card card-sm rounded-2xl bg-base-100 w-96 max-h-100 shadow-sm"
+					>
+						<figure class="relative rounded-2xl min-h-full">
+							<img
+								src={JosephPic}
+								alt="Team member"
+								class="mt-4 object-cover"
+							/>
+							<div
+								class="absolute bottom-0 w-90 bg-base-300/90 mb-2 rounded-2xl p-3 text-center"
+							>
+								<h2 class="font-semibold text-lg">
+									WIAFE, JOSEPH KWAKU
+								</h2>
+								<p class="text-sm mt-1">Frontend Developer</p>
+							</div>
+						</figure>
+					</div>
 
-             
+					<!-- card 2 -->
+					<div
+						class="card card-sm rounded-2xl bg-base-100 w-96 max-h-100 shadow-sm"
+					>
+						<figure class="relative rounded-2xl min-h-full">
+							<img
+								src="https://randomuser.me/api/portraits/women/65.jpg"
+								alt="Team member"
+								class="min-h-full object-cover"
+							/>
+							<div
+								class="absolute bottom-0 w-90 bg-base-300/90 mb-2 rounded-2xl p-3 text-center"
+							>
+								<h2 class="font-semibold text-lg">
+									NORMANYO, LESLIE DELA
+								</h2>
+								<p class="text-sm mt-1">Frontend Developer</p>
+							</div>
+						</figure>
+					</div>
 
-      <!-- Images and names of people -->
-      <div>
-        <p class="text-black text-2xl text-center font-sans font-semibold pt-16 mb-12">
-          Meet Our Team Members.
-        </p>
-        <div class="grid grid-cols-3 grid-rows-3 gap-4 pt-7 ml-16 justify-center">
-          <div class=" relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center hover:scale-105 transition-transform duration-300 w-62 h-62"
-           style="background-image: url('/src/lib/assets/person.jpg'); background-size: cover; background-position: center;">
-           <!-- Overlay for readability-->
-            
-              <div class="relative z-10 flex flex-col items-center justify-end h-full pb-2 w-full">
-                <div class=" bg-white/80 px-7 py-2 rounded w-full text-center mb-2">
-                 <h2 class="text-base font-semibold mb-2 text-yellow-400">John Doe</h2>
-                 <p class="text-gray-600 text-sm">Lead Developer</p>
-                </div>
-             </div>
-          </div>
-           <div class="relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center hover:scale-105 transition-transform duration-300 w-62 h-62" 
-          style="background-image: url('/src/lib/assets/person.jpg'); background-size: cover; background-position: center;">
-          <!-- Overlay for readability-->
-            
-              <div class="relative z-10 flex flex-col items-center justify-end h-64">
-                <div class=" bg-white/70 px-7 py-2 rounded w-full text-center mb-2">
-                  <h2 class="text-base font-semibold mb-1 text-yellow-400">Jane Smith</h2>
-                  <p class="text-gray-600 text-sm">UI/UX Designer</p>
-               </div>
-               </div>
-           </div>
-          <div class="relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center hover:scale-105 transition-transform duration-300 w-62 h-62"
-          style="background-image: url('/src/lib/assets/person.jpg'); background-size: cover; background-position: center;">
-          <!-- Overlay for readability-->
-            
-              <div class="relative z-10 flex flex-col items-center justify-end h-64">
-                <div class=" bg-white/70 px-7 py-2 rounded w-full text-center mb-2">
-                 <h2 class="text-base font-semibold mb-1 text-yellow-400">Alice Johnson Akua</h2>
-                 <p class="text-gray-600 text-sm">Project Manager</p>
-                </div>
-             </div>
-          </div>
-             
-           <div class="relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center hover:scale-105 transition-transform duration-300 w-62 h-62"
-           style="background-image: url('/src/lib/assets/Emma.jpg'); background-size: cover; background-position: center;">
-           <!-- Overlay for readability-->
-          
-                <div class="relative z-10 flex flex-col items-center justify-end h-64">
-                  <div class=" bg-white/70 px-7 py-2 rounded w-full text-center mb-2">
-                     <h2 class="font-semibold mb-1 text-center text-base text-yellow-400">Acquah Emmanuel</h2>
-                     <p class="text-gray-600 text-sm">FrontEnd Developer</p>
-                  </div>
-                 </div>
-             </div>
-          <div class="relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center hover:scale-105 transition-transform duration-300 w-62 h-62"
-          style="background-image: url('/src/lib/assets/Joseph.jpg'); background-size: cover; background-position: center;">
-          <!-- Overlay for readability-->
-          
-              <div class="relative z-10 flex flex-col items-center justify-end h-64">
-                <div class=" bg-white/70 px-7 py-2 rounded w-full text-center mb-2">
-                  <h2 class="text-base font-semibold mb-2 text-yellow-400">Joseph Kwaku Wiafe</h2>
-                 <p class="text-gray-600 text-sm">FrontEnd/UI</p>
-                </div>
-             </div>
-          </div>
-          <div class="relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center hover:scale-105 transition-transform duration-300 w-62 h-62"
-          style="background-image: url('/src/lib/assets/person.jpg'); background-size: cover; background-position: center;">
-            <!-- Overlay for redability-->
-          
-              <div class="relative z-10 flex flex-col items-center justify-end h-64">
-                <div class=" bg-white/70 px-7 py-2 rounded w-full text-center mb-2">
-                   <h2 class="text-base font-semibold mb-2 text-yellow-400">Sarah Lee</h2>
-                   <p class="text-gray-600 text-sm">QA Engineer</p>
-                </div>
-             </div>
-          </div>
-          <div class="relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center hover:scale-105 transition-transform duration-300 w-62 h-62"
-          style="background-image: url('/src/lib/assets/person.jpg'); background-size: cover; background-position: center;">
-          <!-- Overlay for redability -->
-            
-              <div class="relative z-10 flex flex-col items-center justify-end h-64">
-                <div class=" bg-white/70 px-7 py-2 rounded w-full text-center mb-2">
-                  <h2 class="text-base font-semibold mb-2 text-yellow-400">Chris Green</h2>
-                   <p class="text-gray-600 text-sm">Support</p>
-                
-                </div>
-              </div>
-          </div>
-          <div class="relative bg-white rounded-lg shadow-lg p-6 flex flex-col items-center hover:scale-105 transition-transform duration-300 w-62 h-62"
-          style="background-image: url('/src/lib/assets/person.jpg'); background-size: cover; background-position: center;">
-            
-              <div class="relative z-10 flex flex-col items-center justify-end h-64">
-                  <div class=" bg-white/70 px-7 py-2 rounded w-full text-center mb-2">
-                      <h2 class="text-base font-semibold mb-2 text-yellow-400">Linda White</h2>
-                     <p class="text-gray-600 text-sm">Database</p>
-                    </div>
-               </div>
-              </div>
-          </div>
-        </div>
-        <div class="flex-1"><p class ="text-black text-center justify-end h-64 mb-3 mt-3">©Group 72.Project</p> </div>
-      </div>
-    </div>
-  </main>
+					<!-- card 3 -->
+					<div
+						class="card card-sm rounded-2xl bg-base-100 w-96 max-h-100 shadow-sm"
+					>
+						<figure class="relative rounded-2xl min-h-full">
+							<img
+								src="https://randomuser.me/api/portraits/men/74.jpg"
+								alt="Team member"
+								class="min-h-full object-cover"
+							/>
+							<div
+								class="absolute bottom-0 w-90 bg-base-300/90 mb-2 rounded-2xl p-3 text-center"
+							>
+								<h2 class="font-semibold text-lg">
+									AHIAYIBOR, MAWUFEMOR
+								</h2>
+								<p class="text-sm mt-1">Backend Developer</p>
+							</div>
+						</figure>
+					</div>
+
+					<!-- card 4 -->
+					<div
+						class="card card-sm rounded-2xl bg-base-100 w-96 max-h-100 shadow-sm"
+					>
+						<figure class="relative rounded-2xl min-h-full">
+							<img
+								src="https://randomuser.me/api/portraits/women/24.jpg"
+								alt="Team member"
+								class="min-h-full object-cover"
+							/>
+							<div
+								class="absolute bottom-0 w-90 bg-base-300/90 mb-2 rounded-2xl p-3 text-center"
+							>
+								<h2 class="font-semibold text-lg">
+									SARFO, MICHELLE N. A. ASANTEWAA
+								</h2>
+								<p class="text-sm mt-1">Frontend Developer</p>
+							</div>
+						</figure>
+					</div>
+
+					<!-- card 5 -->
+					<div
+						class="card card-sm rounded-2xl bg-base-100 w-96 max-h-100 shadow-sm"
+					>
+						<figure class="relative rounded-2xl min-h-full">
+							<img
+								src="https://randomuser.me/api/portraits/men/18.jpg"
+								alt="Team member"
+								class="min-h-full object-cover"
+							/>
+							<div
+								class="absolute bottom-0 w-90 bg-base-300/90 mb-2 rounded-2xl p-3 text-center"
+							>
+								<h2 class="font-semibold text-lg">
+									WIAFE, RICHMOND KOFI
+								</h2>
+								<p class="text-sm mt-1 text-alert">
+									REFUSED TO PARTICIPATE
+								</p>
+							</div>
+						</figure>
+					</div>
+
+					<!-- card 6 -->
+					<div
+						class="card card-sm rounded-2xl bg-base-100 w-96 max-h-100 shadow-sm"
+					>
+						<figure class="relative rounded-2xl min-h-full">
+							<img
+								src="https://randomuser.me/api/portraits/women/56.jpg"
+								alt="Team member"
+								class="min-h-full object-cover"
+							/>
+							<div
+								class="absolute bottom-0 w-90 bg-base-300/90 mb-2 rounded-2xl p-3 text-center"
+							>
+								<h2 class="font-semibold text-lg">
+									AKPA, CLIFF ANUM
+								</h2>
+								<p class="text-sm mt-1">Full-Stack Developer</p>
+							</div>
+						</figure>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
