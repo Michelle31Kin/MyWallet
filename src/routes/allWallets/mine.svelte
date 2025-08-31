ddfc9474ded3643e1c28b7612a212edafc1ea2db
SARFOMichelle <michellesarfo06@icloud.com>
2025-08-31 14:24:23 +0000

    commiting the changes

---
+++ src/routes/allWallets/mine.svelte
@@ -0,0 +1,18 @@
+ain>
+    <p class = "font-bold justify-center shadow-2xl text-7xl p-64">This is the all wallets page</p>
+
+ <div class="card  bg-info-content w-60 shadow-sm">
+  <figure>
+    <img
+      src="https://img.freepik.com/premium-photo/money-wallet-cartoon-vector-icons-illustration-flat-cartoon-concept-suitable-any-creative-project_839035-1767127.jpg"
+      alt="Wallet" />
+  </figure>
+  <div class="Class body 1x3 font-bold text-neutral-content p-4">
+    <h2 class="Class title 1x3 font-bold">WALLET 1</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-neutral">OPEN</button>
+    </div>
+  </div>
+</div>
+</main>
