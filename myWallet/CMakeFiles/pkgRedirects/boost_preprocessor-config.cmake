e3bdc9aeb5cad29d09716e3171983cbc32106309
Enam A.-M. KODJOH-KPAKPASSOU <eakodjoh-kpakpassou@st.ug.edu.gh>
2025-09-05 18:55:45 +0000

    Delete myWallet directory

--- myWallet/CMakeFiles/pkgRedirects/boost_preprocessor-config.cmake
+++
@@ -1,2 +0,0 @@
-include("/home/enam/data/UG MS ENAM/L100/SECOND SEMESTER/LECTURES/DCIT 104/SEMESTER PROJECT/myWallet/_deps/rebind-src/boost_preprocessor-extra.cmake" OPTIONAL)
-include("/home/enam/data/UG MS ENAM/L100/SECOND SEMESTER/LECTURES/DCIT 104/SEMESTER PROJECT/myWallet/_deps/rebind-src/boost_preprocessorExtra.cmake" OPTIONAL)
